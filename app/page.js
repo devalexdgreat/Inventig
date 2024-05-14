024d650e45ab75eae052c69082804d6c2eb6f08c
devadg <opponamer@gmail.com>
2024-05-15 00:25:43 +0100

    added cool gradient

--- app/page.js
+++ app/page.js
@@ -18,11 +18,11 @@ export default function Home() {
   return (
     <main className="w-full">
       <Navbar />
-      <div className='w-full pt-12 relative'>
+      <div className='w-full md:pt-12 pt-6 relative h-screen md:h-auto flex justify-center items-center'>
 
-      <div className="-z-10 absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
+        <div className="-z-10 absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
 
-      <div className="absolute inset-0 -z-20 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#eee_100%)]"></div>
+        <div className="absolute inset-0 -z-20 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,orange_100%)]"></div>
 
         <div className='w-11/12 mx-auto py-32'>
           <div className='w-full md:w-9/12 mx-auto flex flex-col justify-center'>
